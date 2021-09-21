56e7032659a75119d3f492bde1c4fc3271daa084
vedanthwhitehatjr <86903962+vedanthwhitehatjr@users.noreply.github.com>
2021-09-21 19:33:42 +0530

    Add files via upload

---
+++ js/Base.js
@@ -0,0 +1,20 @@
+class Base{
+    constructor(x,y,width,height,color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height=height;
+        this.color = color;
+        this.body = Bodies.rectangle(x,y,this.width,this.height,this.color);
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        fill("blue");
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
